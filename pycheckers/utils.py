574dae5cdb2a9843077939b925d7c1118aa39f03
Mateusz Ciszczoń <contact@mciszczon.pl>
2018-01-16 19:26:07 +0100

    Updated validation, selecting and moving pieces works.
    Signed-off-by: Mateusz Ciszczoń <contact@mciszczon.pl>

--- pycheckers/utils.py
+++ pycheckers/utils.py
@@ -1,6 +1,12 @@
+"""
+This is Utils module.
+It stores basic functions that allow the game to work.
+TODO: Prepare better architecture, i.e. store each object in its own file.
+"""
+
 class Board:
     """
-    Stores the basic board set up.
+    Stores the basic board setup, i.e. the dimensions.
     """
     columns = rows = 8
     
@@ -11,25 +17,36 @@ class Board:
     def get_rows(self):
         """
         Returns an iterable list of rows.
+        :return list:
         """
         return range(self.rows)
     
     def get_columns(self):
         """
         Returns an iterable list of columns.
+        :return list:
         """
         return range(self.columns)
 
 
 class Player:
+    """
+    A basic object that stores data about a single player,
+    i.e. where he has got his pieces and which of them are kings.
+    """
     pieces = 12
     positions = {}
+    kings = {}
 
     def __init__(self):
         self.pieces = 12
 
 
 class PlayerWhite(Player):
+    """
+    Inherits from Player class.
+    Stores data about a player playing white pieces.
+    """
     positions = {
         (0, 1), (0, 3), (0, 5), (0, 7),
         (1, 0), (1, 2), (1, 4), (1, 6),
@@ -42,6 +59,10 @@ class PlayerWhite(Player):
 
 
 class PlayerBlack(Player):
+    """
+    Inherits from Player class.
+    Stores data about a player playing black pieces.
+    """
     positions = {
         (5, 0), (5, 2), (5, 4), (5, 6),
         (6, 1), (6, 3), (6, 5), (6, 7),
--- pycheckers/views.py
+++ pycheckers/views.py
@@ -1,7 +1,13 @@
+"""
+This is Views module.
+It allows Flask to run properly, as it stores its routing and template rendering settings.
+"""
 import math
 from pycheckers import app, utils
 from flask import render_template, request, redirect, url_for
 
+# TODO: Move variables from below to a new Game object.
+# TODO: Allow to create multiple instances of the Game object (store as objects in array).
 board_initialized = utils.Board()
 rows = board_initialized.get_rows()
 columns = board_initialized.get_columns()
@@ -9,28 +15,62 @@ columns = board_initialized.get_columns()
 player_black = utils.PlayerBlack()
 player_white = utils.PlayerWhite()
 
+turn = {'black', 'white'}
+
 
 @app.route('/')
 def index():
+    """
+    A view that displays the homepage.
+    :return render_template():
+    """
+
     return render_template('index.html')
 
 
 @app.route('/board')
 def board():
+    """
+    A basic view that just generates the board.
+    :return render_template():
+    """
+
     return render_template('board.html')
 
 
 @app.route('/board/select/<int:coordinate_x>/<int:coordinate_y>')
 def select(coordinate_x, coordinate_y):
-    return render_template('board.html', coordinate_x=coordinate_x, coordinate_y=coordinate_y)
+    """
+    A view that handles player selecting one of his pieces.
+    :param int coordinate_x: This is a x coordinate of the piece that player selected.
+    :param int coordinate_y: This is a y coordinate of the piece that player selected.
+    :return render_template() or redirect():
+    """
+
+    # TODO: This check should be more sophisticated with user turns implemented.
+    if coordinate_x in board_initialized.get_rows() and coordinate_y in board_initialized.get_columns():
+        return render_template('board.html', coordinate_x=coordinate_x, coordinate_y=coordinate_y)
+    else:
+        return redirect(url_for('board'))
 
 
 @app.route('/board/select/<int:coordinate_x>/<int:coordinate_y>/move/<int:to_x>/<int:to_y>')
 def move(coordinate_x, coordinate_y, to_x, to_y):
+    """
+    A view that handles moving a piece from one box to another.
+    :param int coordinate_x: This is a x coordinate of the piece we want to move.
+    :param int coordinate_y: This is a y coordinate of the piece we want to move.
+    :param int to_x: This is a x coordinate of the box to which we want to move the piece.
+    :param int to_y: This is a y coordinate of the box to which we want to move the piece.
+    :return redirect():
+    """
 
-    if math.fabs(coordinate_x - to_x) > 1 or math.fabs(coordinate_y - to_y) > 1:
+    if math.fabs(coordinate_x - to_x) != 1 or math.fabs(coordinate_y - to_y) != 1\
+            or to_x not in board_initialized.get_rows() or to_y not in board_initialized.get_columns()\
+            or (to_x, to_y) in player_white.positions or (to_x, to_y) in player_black.positions:
         return redirect(url_for('select', coordinate_x=coordinate_x, coordinate_y=coordinate_y))
 
+    # TODO: This should happen based on which player's turn it is.
     if (coordinate_x, coordinate_y) in player_white.positions:
         player_white.positions.remove((coordinate_x, coordinate_y))
         player_white.positions.add((to_x, to_y))
@@ -43,7 +83,11 @@ def move(coordinate_x, coordinate_y, to_x, to_y):
 
 @app.context_processor
 def inject_variables():
-
+    """
+    Injects variables into every view.
+    Right now it is a very ugly workaround, but just for now.
+    :return dict:
+    """
     return dict(
         rule=str(request.url_rule),
         board={'rows': rows, 'columns': columns},
